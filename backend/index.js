a8d90be2707c6271b2aca43ba0dc2e7f5ca9304c
harshgolyan <harshgolyan308@gmail.com>
2024-11-08 10:57:09 +0530

    backend init

---
+++ backend/index.js
@@ -0,0 +1,12 @@
+const express = require("express")
+const dotenv = require("dotenv")
+const cors = require("cors")
+
+const app = express()
+dotenv.config()
+app.use(cors())
+app.use(express.json())
+
+app.listen(process.env.PORT, () => {
+    console.log(`Server is running on port ${process.env.PORT}`)
+})
