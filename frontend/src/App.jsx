c567e39e537d512a9850fd939e28e68b035e9ae5
harshgolyan <harshgolyan308@gmail.com>
2024-11-09 19:36:46 +0530

    merchant ui

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,6 +5,9 @@ import Otp from './pages/Otp'
 import Dashboard from './pages/Dashboard'
 import TransactionLogs from './pages/TransactionLogs'
 import TotalCredits from './pages/TotalCrerdits'
+import MerchantSignup from './pages/merchant/Signup'
+import MerchantLogin from './pages/merchant/Login'
+import MerchantDashboard from './pages/merchant/MerchantDashboard'
 
 export default function App() {
   return (
@@ -17,6 +20,11 @@ export default function App() {
         <Route path='/dashboard' element={<Dashboard />}/>
         <Route path='/transaction' element={<TransactionLogs />} />
         <Route path='/credits' element={<TotalCredits />}/>
+
+        {/* merchant routes */}
+        <Route path='/merchant/signup' element={<MerchantSignup />} />
+        <Route path='/merchant/login' element={<MerchantLogin />} />
+        <Route path='/merchant/dashboard' element={<MerchantDashboard />} />
       </Routes>
     </BrowserRouter>
     </>
---
+++ frontend/src/pages/merchant/MerchantDashboard.jsx
@@ -0,0 +1,9 @@
+
+
+export default function MerchantDashboard() {
+    return (
+        <>
+            <div>merchant dashboard</div>
+        </>
+    )
+}
